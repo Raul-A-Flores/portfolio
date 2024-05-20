31adc3981a023e579177cc26786a8ffa3d4966f3
Raul Flores <raul.alexander.floress@gmail.com>
2024-05-20 09:09:35 -0700

    base file

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5" >
+    <main className="relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-          ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
